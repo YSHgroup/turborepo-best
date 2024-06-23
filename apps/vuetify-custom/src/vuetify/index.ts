0c395caea432e33d2f8236673f5089c09a30ad8c
YSHgroup <david.dev100@outlook.com>
2024-06-23 08:55:11 -0400

    Configure mdi icon set

--- apps/vuetify-custom/src/vuetify/index.ts
+++ apps/vuetify-custom/src/vuetify/index.ts
@@ -1,7 +1,11 @@
 import 'vuetify/styles'
+import '@mdi/font/css/materialdesignicons.css'
+
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
+import { aliases, mdi } from 'vuetify/iconsets/mdi'
+
 import { lightTheme } from './theme'
 
 export const vuetify = createVuetify({
@@ -10,5 +14,12 @@ export const vuetify = createVuetify({
 	theme: {
 		defaultTheme: 'lightTheme',
 		themes: { lightTheme },
+	},
+	icons: {
+		defaultSet: 'mdi',
+		aliases,
+		sets: {
+			mdi,
+		},
 	},
 })
