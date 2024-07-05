57f207bd493f2be9a50de306a273368a63f25f9c
YSHgroup <david.dev100@outlook.com>
2024-07-05 14:23:10 -0400

    Create PDF generation component

---
+++ apps/vuetify-custom/src/views/business/PdfGeneration.vue
@@ -0,0 +1,57 @@
+<script setup lang="ts">
+	import { ref } from 'vue'
+	import html2pdf from 'html2pdf.js'
+
+
+	const contentToPrint = ref(null)
+
+	const downloadPdf = () => {
+		const element = contentToPrint.value
+		const options = {
+			margin: 1,
+			filename: 'webpage.pdf',
+			image: { type: 'jpeg', quality: 0.98 },
+			html2canvas: { scale: 2 },
+			jsPDF: { unit: 'in', format: 'letter', orientation: 'portrait' },
+		}
+
+		html2pdf().set(options).from(element).save()
+	}
+</script>
+<template>
+	<div>
+		<div
+			ref="contentToPrint"
+			id="pdf-content"
+		>
+			<h1>Business page</h1>
+			<p>This content will be converted to PDF.</p>
+
+			<v-row class="ma-0">
+				<v-col
+					cols="12"
+					sm="6"
+				>
+				</v-col>
+				<v-col
+					cols="12"
+					sm="6"
+				></v-col>
+			</v-row>
+		</div>
+		<v-btn
+			color="primary"
+			prepend-icon="mdi-download-circle"
+			variant="tonal"
+			class="text-subtitle-1"
+			@click="downloadPdf"
+			>Download as PDF</v-btn
+		>
+	</div>
+</template>
+
+<style scoped lang="scss">
+	#pdf-content {
+		text-align: center;
+	}
+</style>
