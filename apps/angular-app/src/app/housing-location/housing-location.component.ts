d00c87c993a93eac7b7dafd54d7c7e1c7eb029c7
YSHgroup <david.dev100@outlook.com>
2024-07-17 14:22:03 -0400

    Create housing-location component

---
+++ apps/angular-app/src/app/housing-location/housing-location.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-housing-location',
+  standalone: true,
+  imports: [],
+  templateUrl: './housing-location.component.html',
+  styleUrl: './housing-location.component.scss'
+})
+export class HousingLocationComponent {
+
+}
