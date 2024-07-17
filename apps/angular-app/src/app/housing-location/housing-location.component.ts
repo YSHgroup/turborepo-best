2e571c707769e9c8fc4695d0f34f879ffd78ff84
YSHgroup <david.dev100@outlook.com>
2024-07-17 17:27:10 -0400

    Add input parameter to component

--- apps/angular-app/src/app/housing-location/housing-location.component.ts
+++ apps/angular-app/src/app/housing-location/housing-location.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { HousingLocation } from '../housinglocation';
 
 @Component({
   selector: 'app-housing-location',
@@ -8,5 +9,5 @@ import { Component } from '@angular/core';
   styleUrl: './housing-location.component.scss'
 })
 export class HousingLocationComponent {
-
+  @Input() housingLocation!: HousingLocation
 }
