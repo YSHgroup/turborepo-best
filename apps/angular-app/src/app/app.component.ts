5295130f78b833945c4bb5044e06d2b51750382b
YSHgroup <david.dev100@outlook.com>
2024-07-22 13:00:40 -0400

    Add nav items and change router

--- apps/angular-app/src/app/app.component.ts
+++ apps/angular-app/src/app/app.component.ts
@@ -3,15 +3,35 @@ import { RouterLink, RouterModule, RouterOutlet } from '@angular/router';
 
 import { HomeComponent } from './home/home.component';
 import { SideNavComponent } from './layouts/side-nav/side-nav.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterModule, HomeComponent, SideNavComponent],
+  imports: [CommonModule, RouterModule, HomeComponent, SideNavComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'homes';
+  navlinks = [
+    {
+      name: 'Housing',
+      path: '/housing'
+    },
+    {
+      name: 'Kanban',
+      path: '/kanban'
+    },
+    {
+      name: 'Housing',
+      path: '#'
+    },
+    {
+      name: 'Housing',
+      path: '#'
+    }
+  ]
+
 }
   
