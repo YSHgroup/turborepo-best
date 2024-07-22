a673f9cf5cce036e35e6bfd6e40007e5d42f3d76
YSHgroup <david.dev100@outlook.com>
2024-07-22 08:11:36 -0400

    Compose initial layout

--- apps/angular-app/src/app/app.component.ts
+++ apps/angular-app/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterLink, RouterModule, RouterOutlet } from '@angular/router';
 
 import { HomeComponent } from './home/home.component';
+import { SideNavComponent } from './layouts/side-nav/side-nav.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterModule, HomeComponent],
+  imports: [RouterModule, HomeComponent, SideNavComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
