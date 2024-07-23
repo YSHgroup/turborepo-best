a477b12aec6a4fd3d1839a76271563f33a47bcf0
Rose <serene.dream218@outlook.com>
2024-07-23 16:46:34 -0400

    Create kanbanboard component and configure bootstrap into angular json

---
+++ apps/angular-app/src/app/kanban/kabanboard/kabanboard.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-kabanboard',
+  standalone: true,
+  imports: [],
+  templateUrl: './kabanboard.component.html',
+  styleUrl: './kabanboard.component.scss'
+})
+export class KabanboardComponent {
+
+}
