7c55475920c67fd9dce816af6cf9aeebd3fd72d7
Rose <serene.dream218@outlook.com>
2024-07-23 17:59:50 -0400

    Add elements into task item component

--- apps/angular-app/src/app/kanban/kanban-main/kanban-main.component.ts
+++ apps/angular-app/src/app/kanban/kanban-main/kanban-main.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { KabanboardComponent } from '../kabanboard/kabanboard.component';
+import { TaskItemComponent } from '../task-item/task-item.component';
 
 @Component({
   selector: 'app-kanban-main',
   standalone: true,
-  imports: [KabanboardComponent],
+  imports: [KabanboardComponent, TaskItemComponent],
   templateUrl: './kanban-main.component.html',
   styleUrl: './kanban-main.component.scss'
 })
