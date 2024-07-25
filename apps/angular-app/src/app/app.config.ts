86b7152d7ee3c9c1bf8fec41a35d4e23a6ee33dd
Rose <serene.dream218@outlook.com>
2024-07-25 18:11:40 -0400

    Add transition for each task card

--- apps/angular-app/src/app/app.config.ts
+++ apps/angular-app/src/app/app.config.ts
@@ -1,8 +1,9 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async'
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAnimationsAsync()]
 };
--- apps/angular-app/src/app/kanban/task-item/task-item.component.ts
+++ apps/angular-app/src/app/kanban/task-item/task-item.component.ts
@@ -1,21 +1,44 @@
 import { Component, Input } from '@angular/core';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import {faCaretUp} from '@fortawesome/free-solid-svg-icons'
-import { TaskModel } from '../../models/kaban';
 import { CommonModule } from '@angular/common';
+import {
+  animate,
+  trigger,
+  state,
+  style,
+  transition,
+} from '@angular/animations';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faCaretUp } from '@fortawesome/free-solid-svg-icons';
+
+import { TaskModel } from '../../models/kaban';
 
 @Component({
   selector: 'app-task-item',
   standalone: true,
   imports: [CommonModule, FontAwesomeModule],
+  animations: [
+    trigger('toggleTask', [
+      state('open', style({
+        height: 'fit-content'
+      })),
+      state('closed', style({
+        height: '0px',
+        overflow: 'hidden'
+      })),
+      transition('open => closed, closed => open', [animate('0.2s ease-in')]),
+    ]),
+  ],
   templateUrl: './task-item.component.html',
-  styleUrl: './task-item.component.scss'
+  styleUrl: './task-item.component.scss',
 })
 export class TaskItemComponent {
-  @Input({required: true}) task?: TaskModel
-  faCaretUp = faCaretUp
+  @Input({ required: true }) task?: TaskModel;
+  faCaretUp = faCaretUp;
+  isOpen = true;
 
-  constructor() {
+  constructor() {}
+
+  toggle() {
+    this.isOpen = !this.isOpen;
   }
-
 }
