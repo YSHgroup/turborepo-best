21847701c2f93cf581d007b0aa104a8c768992cb
Rose <serene.dream218@outlook.com>
2024-07-26 14:18:58 -0400

    Create task modal and add open functionality

--- apps/angular-app/src/app/kanban/kabanboard/kabanboard.component.ts
+++ apps/angular-app/src/app/kanban/kabanboard/kabanboard.component.ts
@@ -1,20 +1,43 @@
 import { Component, Input } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faPlus } from '@fortawesome/free-solid-svg-icons';
-
+import Swal from 'sweetalert2/dist/sweetalert2.js';
+import 'sweetalert2/src/sweetalert2.scss';
+import { SweetAlert2Module } from '@sweetalert2/ngx-sweetalert2';
 import { KabanBoardModel } from '../../models/kaban';
+import { NewTaskComponent } from '../modal/new-task/new-task.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-kabanboard',
   standalone: true,
-  imports: [FontAwesomeModule],
+  imports: [CommonModule, FontAwesomeModule, SweetAlert2Module, NewTaskComponent],
   host: {
-    'class': 'card m-2 kanban-board shadow-sm'
+    class: 'card m-2 kanban-board shadow-sm',
   },
   templateUrl: './kabanboard.component.html',
-  styleUrl: './kabanboard.component.scss'
+  styleUrl: './kabanboard.component.scss',
 })
 export class KabanboardComponent {
-  @Input({required: true}) taskBoard?: KabanBoardModel
-  faPlus = faPlus
+  @Input({ required: true }) taskBoard?: KabanBoardModel;
+  faPlus = faPlus;
+  isTaskModalOpen = false
+
+  newTask() {
+    this.isTaskModalOpen = true
+  }
+
+  closeTaskModal() {
+    this.isTaskModalOpen = false
+  }
+
+  fireSwal() {
+    Swal.fire({
+      title: 'New task',
+      input: 'text',
+      inputLabel: 'Task title',
+      confirmButtonText: 'Confirm',
+      iconColor: 'yellowgreen',
+    }).then((result) => console.log('value: ', result));
+  }
 }
