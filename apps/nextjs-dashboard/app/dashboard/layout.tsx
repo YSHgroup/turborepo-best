2525952946c5740438639a2aaaa0186cc91ba0ef
YSHgroup <david.dev100@outlook.com>
2024-08-12 16:28:21 -0400

    Enable partial rendering - PPR

--- apps/nextjs-dashboard/app/dashboard/layout.tsx
+++ apps/nextjs-dashboard/app/dashboard/layout.tsx
@@ -1,5 +1,7 @@
 import SideNav from '../ui/dashboard/sidenav'
 
+export const experimental_ppr = true;
+
 export default function layout({ children }: { children: React.ReactNode }) {
 	return (
 		<div className='flex h-screen flex-col md:flex-row md:overflow-hidden'>
