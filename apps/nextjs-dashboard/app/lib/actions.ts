17b158716affd82b310baa0ee4451d8f5b9f02a3
YSHgroup <david.dev100@outlook.com>
2024-08-13 13:46:33 -0400

    Create invoice-update page

--- apps/nextjs-dashboard/app/lib/actions.ts
+++ apps/nextjs-dashboard/app/lib/actions.ts
@@ -32,3 +32,25 @@ export async function createInvoice(formData: FormData) {
   revalidatePath('dashboard/invoices')
   redirect('dashboard/invoices')
 }
+
+// Use Zod to update the expected types
+const UpdateInvoice = FormSchema.omit({ id: true, date: true });
+ 
+export async function updateInvoice(id: string, formData: FormData) {
+  const { customerId, amount, status } = UpdateInvoice.parse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  });
+ 
+  const amountInCents = amount * 100;
+ 
+  await sql`
+    UPDATE invoices
+    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+    WHERE id = ${id}
+  `;
+ 
+  revalidatePath('/dashboard/invoices');
+  redirect('/dashboard/invoices');
+}
