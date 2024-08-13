c9aa8ec5472d66ac2b689e8b47d8e1da1dbb2452
YSHgroup <david.dev100@outlook.com>
2024-08-13 13:50:38 -0400

    Add delete feature

--- apps/nextjs-dashboard/app/lib/actions.ts
+++ apps/nextjs-dashboard/app/lib/actions.ts
@@ -54,3 +54,8 @@ export async function updateInvoice(id: string, formData: FormData) {
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
 }
+
+export async function deleteInvoice(id: string) {
+	await sql`DELETE FROM invoices WHERE id = ${id}`;
+	revalidatePath('/dashboard/invoices');
+  }
