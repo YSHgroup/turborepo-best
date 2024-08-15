459ee130644190c393ca1290f82fdbc07b79fdb3
YSHgroup <david.dev100@outlook.com>
2024-08-14 22:58:41 -0400

    Update metadata

--- apps/nextjs-dashboard/app/layout.tsx
+++ apps/nextjs-dashboard/app/layout.tsx
@@ -9,7 +9,19 @@ export const metadata: Metadata = {
   },
   keywords: 'Next.js tutorial, Next.js dashboard, Next.js app router',
   description: 'The official Next.js Course Dashboard, built with App Router.',
-  metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
+  metadataBase: new URL('https://next-tutorial-yshgroup.vercel.app'),
+  authors: [{name: 'David Wilson', url:'https://github.com/YSHgroup'}],
+  openGraph: {
+    title: 'Acme Dashboard',
+    description: 'The official Next.js Learn Dashboard built with App Router.',
+    url: 'https://next-tutorial-yshgroup.vercel.app',
+    type: 'website',
+  },
+  twitter: {
+    site: '@acme',
+    description:'The official Next.js Learn Dashboard built with App Router.',
+    title:'Acme Dashboard'
+  },
 };
 
 export default function RootLayout({
