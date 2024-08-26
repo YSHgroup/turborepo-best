ba97ac466300fba729f9c33c8b11a5b1fd33b3c0
YSHgroup <david.dev100@outlook.com>
2024-08-26 05:19:28 -0400

    Add exactly once logic

--- apps/backend/src/chat-server/index.ts
+++ apps/backend/src/chat-server/index.ts
@@ -18,17 +18,22 @@ io.emit('hello', 'everyone')
 io.on('connection', async (socket) => {
   console.log('a new client connected: ', socket)
 
-  socket.on('chat message', async (msg) => {
+  socket.on('chat message', async (msg, clientOffset, callback) => {
     console.log('Client sent a message: ', msg)
     let result
     try {
-      result = await db.run('INSERT INTO messages (content) VALUES (?)', msg)
-    } catch (error) {
+      result = await db.run('INSERT INTO messages (content, client_offset) VALUES (?)', msg, clientOffset)
+    } catch (error: any) {
       console.error(error)
+      if(error.errno === 19 /* SQLITE_CONSTRAINT */ ) {
+        // the message was already inserted, so we notify the client
+        callback()
+      }
       return
     }
     
     io.emit('chat message', msg, result.lastID) // send the message including the sender
+    callback()
   })
 
   socket.broadcast.emit('Hi') // broadcast to everyone except for a certain emitting socket
