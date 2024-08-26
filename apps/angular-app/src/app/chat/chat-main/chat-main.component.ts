63ad9ecff29ea6e67580e4ca51e704841c932094
YSHgroup <david.dev100@outlook.com>
2024-08-26 11:56:42 -0400

    Create chat message component and add styles

--- apps/angular-app/src/app/chat/chat-main/chat-main.component.ts
+++ apps/angular-app/src/app/chat/chat-main/chat-main.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
-import { ChatInputComponent } from "../chat-input/chat-input.component";
+import { ChatInputComponent } from '../chat-input/chat-input.component';
+import { ChatMessageComponent } from '../chat-message/chat-message.component';
 
 @Component({
   selector: 'app-chat-main',
   standalone: true,
-  imports: [ChatInputComponent],
+  imports: [ChatInputComponent, ChatMessageComponent],
   templateUrl: './chat-main.component.html',
-  styleUrl: './chat-main.component.scss'
+  styleUrl: './chat-main.component.scss',
 })
-export class ChatMainComponent {
-
-}
+export class ChatMainComponent {}
---
+++ apps/angular-app/src/app/chat/chat-message/chat-message.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-chat-message',
+  standalone: true,
+  imports: [],
+  templateUrl: './chat-message.component.html',
+  styleUrl: './chat-message.component.scss'
+})
+export class ChatMessageComponent {
+
+}
