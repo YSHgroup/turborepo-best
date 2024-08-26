8c41b5e62e3877cbaefc6eebf424a0aa8b1288a2
YSHgroup <david.dev100@outlook.com>
2024-08-26 00:34:24 -0400

    Add logic to insert user into db and sending logic for missing message

--- apps/backend/src/chat-server/index.ts
+++ apps/backend/src/chat-server/index.ts
@@ -1,7 +1,9 @@
 import { Server } from 'socket.io'
 import { dbConnect } from '@/services/sqliteConnect'
+import { Database } from 'sqlite'
+import sqlite3 from 'sqlite3'
 
-let db
+let db: Database<sqlite3.Database, sqlite3.Statement>
 (async function() {
   db = await dbConnect()
 })()
@@ -13,16 +15,36 @@ const io = new Server(5000, {
 // this will emit the event to all connected sockets
 io.emit('hello', 'everyone')
 
-io.on('connection', (socket) => {
+io.on('connection', async (socket) => {
   console.log('a new client connected: ', socket)
 
-  socket.on('chat message', (msg) => {
+  socket.on('chat message', async (msg) => {
     console.log('Client sent a message: ', msg)
+    let result
+    try {
+      result = await db.run('INSERT INTO messages (content) VALUES (?)', msg)
+    } catch (error) {
+      console.error(error)
+      return
+    }
     
-    io.emit('chat message', msg) // send the message including the sender
+    io.emit('chat message', msg, result.lastID) // send the message including the sender
   })
 
   socket.broadcast.emit('Hi') // broadcast to everyone except for a certain emitting socket
+
+  if(!socket.recovered) {
+    try{
+      await db.each('SELECT id, content FROM  messages WHERE id > ?',
+        [socket.handshake.auth.serverOffset || 0],
+        (_error, row) => {
+          socket.emit('chat message', row.content, row.id)
+        }
+      )
+    } catch(e) {
+
+    }
+  }
 
   socket.on('disconnect', () => {
     console.log('a client disconnected')
